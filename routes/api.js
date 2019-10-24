d42dca1d90f76ada1f575d8f0d61789b14f28384
Shawn Li <lxiange@cmu.edu>
2019-10-23 20:37:30 -0700

    finish remaining api

--- routes/api.js
+++ routes/api.js
@@ -32,7 +32,8 @@ router.get('/paper', async (req, res, next) => {
             venue: paper.venue,
             abstract: paper.abstract,
             pages: paper.pages,
-            citations: citations
+            citations: citations,
+            volume: paper.volume
         };
         console.log('res is', body);
         res.status(200).json(body);
@@ -130,6 +131,117 @@ router.get('/person/coworkers', async (req, res) => {
 })
 
 
+router.get('/channel', async (req, res) => {
+    try {
+        const name = req.query.name;
+        console.log('name', name);
+
+        const result = await session.run(
+            `Match (p1:Person)-[:Writes]->(p2:Paper) where p2.venue=$name With p2.volume as volume,p1 return volume,count(p1) as counts`, {
+                name: name
+            }
+        );
+        console.log(result.records);
+        if (result.records.length === 0) {
+            res.sendStatus(404);
+            return;
+        }
+        console.log(result.records);
+        const body = {
+            name: name,
+            volumes: result.records.map(x => {
+                return {name: x.get('volume'), authorCount: x.get('counts')}
+            })
+        }
+        res.status(200).json(body);
+    } catch (e) {
+        console.log('error', e);
+        res.status(400).json({error: e});
+    }
+});
+
+router.get('/search/experts', async (req, res) => {
+    try {
+
+        const key = req.query.key;
+        console.log('key', key);
+
+        const result = await session.run(
+            `Match (p2:Person) WHERE p2.interests CONTAINS $key return p2 as person`,
+            {
+                key: key
+            }
+        );
+        console.log(result.records);
+        if (result.records.length === 0) {
+            res.sendStatus(404);
+            return;
+        }
+        const personList = result.records.map(x => {
+            const person = x.get('person').properties
+            if (!person.interests) {
+                person.interests = ''
+            }
+            return {
+                name: person.name,
+                photoUrl: person.photo,
+                researchInterests: person.interests.split(', '),
+                affiliation: person.affiliation,
+                email: person.email,
+            }
+        })
+        console.log(personList)
+        res.status(200).json(personList);
+    } catch (e) {
+        console.log('error', e);
+        res.status(400).json({error: e});
+    }
+
+});
+
+router.get('/search/focused_topics', async (req, res) => {
+    try {
+        const channelName = req.query.channelName;
+        const year = req.query.year;
+        console.log('channelName', channelName);
+
+        const result = await session.run(
+            `Match (p2:Paper) where p2.venue=$channelName and p2.year=$year return p2.title as title`,
+            {
+                channelName: channelName,
+                year: year
+            }
+        );
+        console.log(result.records);
+        if (result.records.length === 0) {
+            res.sendStatus(404);
+            return;
+        }
+
+        const countMap = new Map();
+
+        result.records.forEach(p => {
+            const title = p.get('title');
+            title.split(' ').forEach(word => {
+                if (!countMap.has(word)) {
+                    countMap.set(word, 0);
+                }
+                countMap.set(word, countMap.get(word) + 1);
+            })
+        })
+        countMap[Symbol.iterator] = function* () {
+            yield* [...this.entries()].sort((a, b) => (a[1] - b[1] || b[0].length - a[0].length));
+        }
+
+
+        const body = [...countMap].slice(0, 20);
+        console.log(body)
+        res.status(200).json(body);
+    } catch (e) {
+        console.log('error', e);
+        res.status(400).json({error: e});
+    }
+});
 
 
 module.exports = router;
