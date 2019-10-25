07d40c24b265fce86618f0551edd494463de12a8
Shawn Li <lxiange@cmu.edu>
2019-10-24 22:22:23 -0700

    move index.html to /

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Frontend')));
 app.use(cors());
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api',apiRouter);
 
